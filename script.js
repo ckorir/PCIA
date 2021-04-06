ca3a9250c79ba3ed5a5537f7a4d42eae9245682a
ckorir <chrisruto.k@gmail.com>
2021-04-06 14:04:37 +0300

    changed menu js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const burger = document.querySelector('#toggle');
 const info = document.querySelector('.menu');
 
 burger.addEventListener('change',function(e){
-    if(burger.checked){
+    if(burger.notchecked){
         info.style.display = 'none';
     }else{
         info.style.display = 'initial';
